6b34237c9434d3fbe5b767e7de92ecea0261d16d
John serrano <andrey_201015@hotmail.com>
2017-10-09 17:56:33 -0500

    Terminando secciones generales

--- sites/default/themes/clasificados/page--taxonomy.tpl.php
+++ sites/default/themes/clasificados/page--taxonomy.tpl.php
@@ -1,1 +1,30 @@
-<h3>Seccion general</h3>
+<section id="seccionGeneral">
+
+  <article id="categorias" class="container">
+    <h4>Cateogiras</h4>
+  </article>
+
+  <article id="noticias" class="container">
+    <hr>
+    <h3>Noticias</h3>
+    <?php $view = views_get_view('categorias'); print $view->preview('block_3'); ?>
+  </article>
+
+  <article id="anunciosGeneral" class="container">
+    <?php $view = views_get_view('categorias'); print $view->preview('block'); ?>
+  </article>
+
+  <div class="pauta"></div>
+
+  <article id="masGeneral" class="container">
+    <?php $view = views_get_view('categorias'); print $view->preview('block_10'); ?>
+  </article>
+
+  <article id="otrosAnuncios" class="container">
+    <hr>
+    <aside class="estrellas"></aside>
+    <h3>También te puede interesar</h3>
+    <?php $view = views_get_view('categorias'); print $view->preview('block_5'); ?>
+  </article>
+
+</section>
