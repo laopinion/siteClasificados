2396c66ed2ca103d977e704c8f6860d23fcb870c
John serrano <andrey_201015@hotmail.com>
2017-10-18 17:47:57 -0500

    Modificando el tpl de noticias

---
+++ sites/default/themes/clasificados/node--article.tpl.php
@@ -0,0 +1,8 @@
+<section id="noticia">
+  <div id="noticiaPortada">
+    <?php print render($content['field_images']); ?>
+  </div>
+  <h2 id="noticiaTitle" class="container">
+    <?php print $title; ?>
+  </h2>
+</section>
